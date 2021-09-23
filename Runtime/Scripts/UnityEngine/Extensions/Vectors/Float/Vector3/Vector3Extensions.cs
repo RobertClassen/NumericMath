5c6f99f0a1c162766e89aa93ea108078672878c4
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-09-23 22:37:55 +0200

    added 2d versions of Dot method for Vector3

--- Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector3/Vector3Extensions.cs
+++ Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector3/Vector3Extensions.cs
@@ -61,6 +61,21 @@
 			return Vector3.Dot(vector, other);
 		}
 
+		public static float DotXY(this Vector3 vector, Vector3 other)
+		{
+			return vector.x * other.x + vector.y + other.y;
+		}
+
+		public static float DotXZ(this Vector3 vector, Vector3 other)
+		{
+			return vector.x * other.x + vector.z + other.z;
+		}
+
+		public static float DotYZ(this Vector3 vector, Vector3 other)
+		{
+			return vector.y * other.y + vector.z + other.z;
+		}
+
 		public static Vector3 Floor(this Vector3 vector)
 		{
 			return new Vector3(vector.x.Floor(), vector.y.Floor(), vector.z.Floor());
