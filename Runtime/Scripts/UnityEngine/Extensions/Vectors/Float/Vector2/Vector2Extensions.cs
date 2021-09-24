b17c5f86430fee35b1e1efb32453f0f77f31e03c
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-09-25 00:16:16 +0200

    added MoveTowards and SmoothDamp methods

--- Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector2/Vector2Extensions.cs
+++ Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector2/Vector2Extensions.cs
@@ -102,11 +102,48 @@
 		}
 
 		/// <summary>
+		/// Moves the point current towards the <c>target</c>.
+		/// </summary>
+		/// <remarks>
+		/// See https://docs.unity3d.com/ScriptReference/Vector2.MoveTowards.html
+		/// </remarks>
+		public static Vector2 MoveTowards(this Vector2 point, Vector2 target, float maxDistanceDelta)
+		{
+			return Vector2.MoveTowards(point, target, maxDistanceDelta);
+		}
+
+		/// <summary>
 		/// Performs a component-wise `Sign` operation.
 		/// </summary>
 		public static Vector2 Sign(this Vector2 vector)
 		{
 			return new Vector2(vector.x.Sign(), vector.y.Sign());
+		}
+
+		/// <summary>
+		/// Gradually changes the point towards the <c>target</c> over time.
+		/// </summary>
+		/// <remarks>
+		/// See https://docs.unity3d.com/ScriptReference/Vector2.SmoothDamp.html
+		/// </remarks>
+		public static Vector2 SmoothDamp(this Vector2 point, Vector2 target, 
+			ref Vector2 currentVelocity, float smoothTime, 
+			float maxSpeed = Mathf.Infinity)
+		{
+			return Vector2.SmoothDamp(point, target, ref currentVelocity, smoothTime, maxSpeed, Time.deltaTime);
+		}
+
+		/// <summary>
+		/// Gradually changes the point towards the <c>target</c> over time.
+		/// </summary>
+		/// <remarks>
+		/// See https://docs.unity3d.com/ScriptReference/Vector2.SmoothDamp.html
+		/// </remarks>
+		public static Vector2 SmoothDamp(this Vector2 point, Vector2 target, 
+			ref Vector2 currentVelocity, float smoothTime, 
+			float maxSpeed, float deltaTime)
+		{
+			return Vector2.SmoothDamp(point, target, ref currentVelocity, smoothTime, maxSpeed, deltaTime);
 		}
 		#endregion
 	}
--- Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector3/Vector3Extensions.cs
+++ Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector3/Vector3Extensions.cs
@@ -104,11 +104,48 @@
 		}
 
 		/// <summary>
+		/// Moves the point current towards the <c>target</c>.
+		/// </summary>
+		/// <remarks>
+		/// See https://docs.unity3d.com/ScriptReference/Vector3.MoveTowards.html
+		/// </remarks>
+		public static Vector3 MoveTowards(this Vector3 point, Vector3 target, float maxDistanceDelta)
+		{
+			return Vector3.MoveTowards(point, target, maxDistanceDelta);
+		}
+
+		/// <summary>
 		/// Performs a component-wise `Sign` operation.
 		/// </summary>
 		public static Vector3 Sign(this Vector3 vector)
 		{
 			return new Vector3(vector.x.Sign(), vector.y.Sign(), vector.z.Sign());
+		}
+
+		/// <summary>
+		/// Gradually changes the point towards the <c>target</c> over time.
+		/// </summary>
+		/// <remarks>
+		/// See https://docs.unity3d.com/ScriptReference/Vector3.SmoothDamp.html
+		/// </remarks>
+		public static Vector3 SmoothDamp(this Vector3 point, Vector3 target, 
+			ref Vector3 currentVelocity, float smoothTime, 
+			float maxSpeed = Mathf.Infinity)
+		{
+			return Vector3.SmoothDamp(point, target, ref currentVelocity, smoothTime, maxSpeed, Time.deltaTime);
+		}
+
+		/// <summary>
+		/// Gradually changes the point towards the <c>target</c> over time.
+		/// </summary>
+		/// <remarks>
+		/// See https://docs.unity3d.com/ScriptReference/Vector3.SmoothDamp.html
+		/// </remarks>
+		public static Vector3 SmoothDamp(this Vector3 point, Vector3 target, 
+			ref Vector3 currentVelocity, float smoothTime, 
+			float maxSpeed, float deltaTime)
+		{
+			return Vector3.SmoothDamp(point, target, ref currentVelocity, smoothTime, maxSpeed, deltaTime);
 		}
 		#endregion
 	}
