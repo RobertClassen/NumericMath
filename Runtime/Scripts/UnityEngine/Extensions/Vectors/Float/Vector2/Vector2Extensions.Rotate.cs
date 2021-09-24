63c90835afa7db7cb0ae6514baf2ec11c116bf9c
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-09-24 18:42:37 +0200

    added Rotate methods to Vector2

--- Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector2/Vector2Extensions.Rotate.cs
+++ Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector2/Vector2Extensions.Rotate.cs
@@ -20,6 +20,25 @@ namespace WellDefinedValues
 		#endregion
 
 		#region Methods
+		/// <summary>
+		/// Returns the 2D vector perpendicular to this 2D vector.
+		/// </summary>
+		/// <remarks>
+		/// The result is always rotated 90° in a counter-clockwise direction 
+		/// for a 2D coordinate system where the positive Y axis goes up.
+		/// 
+		/// See https://docs.unity3d.com/ScriptReference/Vector2.Perpendicular.html
+		/// </remarks>
+		[Obsolete("Use 'Rotate90' instead.")]
+		public static Vector2 Perpendicular(this Vector2 vector)
+		{
+			return Vector2.Perpendicular(vector);
+		}
+
+		/// <summary>
+		/// Rotate the vector in a counter-clockwise direction, 
+		/// for a 2D coordinate system where the positive Y axis goes up.
+		/// </summary>
 		public static Vector2 Rotate(this Vector2 point, float degrees)
 		{
 			float radians = degrees * Mathf.Deg2Rad;
@@ -30,9 +49,67 @@ namespace WellDefinedValues
 				point.x * sin + point.y * cos);
 		}
 
+		/// <summary>
+		/// Rotate the vector in a counter-clockwise direction around the <c>center</c>, 
+		/// for a 2D coordinate system where the positive Y axis goes up.
+		/// </summary>
 		public static Vector2 Rotate(this Vector2 point, float degrees, Vector2 center)
 		{
 			return (point - center).Rotate(degrees) + center;
+		}
+
+		/// <summary>
+		/// Rotate the vector in a counter-clockwise direction, 
+		/// for a 2D coordinate system where the positive Y axis goes up.
+		/// </summary>
+		public static Vector2 Rotate90(this Vector2 point)
+		{
+			return new Vector2(-point.y, point.x);
+		}
+
+		/// <summary>
+		/// Rotate the vector in a counter-clockwise direction around the <c>center</c>, 
+		/// for a 2D coordinate system where the positive Y axis goes up.
+		/// </summary>
+		public static Vector2 Rotate90(this Vector2 point, Vector2 center)
+		{
+			return (point - center).Rotate90() + center;
+		}
+
+		/// <summary>
+		/// Rotate the vector in a counter-clockwise direction, 
+		/// for a 2D coordinate system where the positive Y axis goes up.
+		/// </summary>
+		public static Vector2 Rotate180(this Vector2 point)
+		{
+			return new Vector2(-point.x, -point.y);
+		}
+
+		/// <summary>
+		/// Rotate the vector in a counter-clockwise direction around the <c>center</c>, 
+		/// for a 2D coordinate system where the positive Y axis goes up.
+		/// </summary>
+		public static Vector2 Rotate180(this Vector2 point, Vector2 center)
+		{
+			return (point - center).Rotate180() + center;
+		}
+
+		/// <summary>
+		/// Rotate the vector in a counter-clockwise direction, 
+		/// for a 2D coordinate system where the positive Y axis goes up.
+		/// </summary>
+		public static Vector2 Rotate270(this Vector2 point)
+		{
+			return new Vector2(point.y, -point.x);
+		}
+
+		/// <summary>
+		/// Rotate the vector in a counter-clockwise direction around the <c>center</c>, 
+		/// for a 2D coordinate system where the positive Y axis goes up.
+		/// </summary>
+		public static Vector2 Rotate270(this Vector2 point, Vector2 center)
+		{
+			return (point - center).Rotate270() + center;
 		}
 		#endregion
 	}
