de5e0d5f83b92b11afd9557de155ad702c3cbafc
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-09-25 21:43:24 +0200

    added Remainder for Double

--- Runtime/Scripts/System/Extensions/Numerics/Double/DoubleExtensions.Modulo.cs
+++ Runtime/Scripts/System/Extensions/Numerics/Double/DoubleExtensions.Modulo.cs
@@ -6,6 +6,26 @@ namespace WellDefinedValues
 
 	public static partial class DoubleExtensions
 	{
+		/// <remarks>
+		/// Repeats the sequence from <c>zero</c> to <c>divisor</c> (exclusively), 
+		/// with the direction and <c>sign</c> based on the <c>sign</c> of the <c>divisor</c>.
+		/// </remarks>
+		/// <seealso cref="Remainder"/>
+		/// <example>
+		/// <code>
+		/// double modulo = 3f;
+		/// double[] input    = { -4.9d, -3.9d, -2.9d, -1.9d, -0.9d, 0.1d, 1.1d, 2.1d, 3.1d, 4.1d, 5.1d };
+		/// double[] expected = {  1.1d,  2.1d,  0.1d,  1.1d,  2.1d, 0.1d, 1.1d, 2.1d, 0.1d, 1.1d, 2.1d };
+		/// double[] actual   = {  1.1d,  2.1d,  0.1d,  1.1d,  2.1d, 0.1d, 1.1d, 2.1d, 0.1d, 1.1d, 2.1d };
+		/// </code>
+		/// 
+		/// <code>
+		/// double modulo = -3f;
+		/// double[] input    = { -4.9d, -3.9d, -2.9d, -1.9d, -0.9d,  0.1d,  1.1d,  2.1d,  3.1d,  4.1d,  5.1d };
+		/// double[] expected = { -1.9d, -0.9d, -2.9d, -1.9d, -0.9d, -2.9d, -1.9d, -0.9d, -2.9d, -1.9d, -0.9d };
+		/// double[] actual   = { -1.9d, -0.9d, -2.9d, -1.9d, -0.9d, -2.9d, -1.9d, -0.9d, -2.9d, -1.9d, -0.9d };
+		/// </code>
+		/// </example>
 		public static double Modulo(this double dividend, double divisor)
 		{
 			if(divisor == Double.Zero)
---
+++ Runtime/Scripts/System/Extensions/Numerics/Double/DoubleExtensions.Remainder.cs
@@ -0,0 +1,35 @@
+namespace WellDefinedValues
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+
+	public static partial class DoubleExtensions
+	{
+		/// <remarks>
+		/// Repeats the sequence from <c>zero</c> to <c>divisor</c> (exclusively), 
+		/// with the direction and <c>sign</c> based on the <c>sign</c> of the <c>dividend</c>.
+		/// </remarks>
+		/// <seealso cref="Modulo"/>
+		/// <example>
+		/// <code>
+		/// double modulo = 3d;
+		/// double[] input    = { -4.9d, -3.9d, -2.9d, -1.9d, -0.9d, 0.1d, 1.1d, 2.1d, 3.1d, 4.1d, 5.1d };
+		/// double[] expected = {  1.1d,  2.1d,  0.1d,  1.1d,  2.1d, 0.1d, 1.1d, 2.1d, 0.1d, 1.1d, 2.1d };
+		/// double[] actual   = { -1.9d, -0.9d, -2.9d, -1.9d, -0.9d, 0.1d, 1.1d, 2.1d, 0.1d, 1.1d, 2.1d }; // Wrong for - % +
+		/// </code>
+		/// 
+		/// <code>
+		/// double modulo = -3d;
+		/// double[] input    = { -4.9d, -3.9d, -2.9d, -1.9d, -0.9d,  0.1d,  1.1d,  2.1d,  3.1d,  4.1d,  5.1d };
+		/// double[] expected = { -1.9d, -0.9d, -2.9d, -1.9d, -0.9d, -2.9d, -1.9d, -0.9d, -2.9d, -1.9d, -0.9d };
+		/// double[] actual   = { -1.9d, -0.9d, -2.9d, -1.9d, -0.9d,  0.1d,  1.1d,  2.1d,  0.1d,  1.1d,  2.1d }; // Wrong for + % -
+		/// </code>
+		/// </example>
+		[Obsolete("Use the '%' operator directly instead.")]
+		public static double Remainder(this double dividend, double divisor)
+		{
+			return dividend % divisor;
+		}
+	}
+}
