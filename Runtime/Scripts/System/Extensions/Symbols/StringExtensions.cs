0b81913418ccefa298f4e115f84294e7f8f22217
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-09-25 20:47:56 +0200

    added SetSlashs methods for string

--- Runtime/Scripts/System/Extensions/Symbols/StringExtensions.cs
+++ Runtime/Scripts/System/Extensions/Symbols/StringExtensions.cs
@@ -3,6 +3,7 @@
 	using System;
 	using System.Collections;
 	using System.Collections.Generic;
+	using System.IO;
 
 	public static class StringExtensions
 	{
@@ -27,6 +28,38 @@
 		public static bool IsNullOrEmpty(this string text)
 		{
 			return string.IsNullOrEmpty(text);
+		}
+
+		/// <summary>
+		/// Replaces platform-specific slashes with their opposite.
+		/// </summary>
+		public static string SetAltSlashes(this string text)
+		{
+			return text.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+		}
+
+		/// <summary>
+		/// Replaces forward slashes with their opposite.
+		/// </summary>
+		public static string SetBackwardSlashes(this string text)
+		{
+			return text.Replace(Char.Slash, Char.Backslash);
+		}
+
+		/// <summary>
+		/// Replaces backward slashes with their opposite.
+		/// </summary>
+		public static string SetForwardSlashes(this string text)
+		{
+			return text.Replace(Char.Backslash, Char.Slash);
+		}
+
+		/// <summary>
+		/// Replaces platform-specific alternate slashes with their opposite.
+		/// </summary>
+		public static string SetSlashes(this string text)
+		{
+			return text.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
 		}
 
 		public static string[] Split(this string text, char separator)
