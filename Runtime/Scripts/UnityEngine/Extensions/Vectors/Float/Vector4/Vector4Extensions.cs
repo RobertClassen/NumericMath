5c5cbf65d3a85c2a8ad663ca16e1c96cdba14ac5
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-10-02 21:33:08 +0200

    split Vector4Extensions

--- Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector4/Vector4Extensions.cs
+++ Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector4/Vector4Extensions.cs
@@ -5,20 +5,8 @@
 	using System.Collections.Generic;
 	using UnityEngine;
 
-	public static class Vector4Extensions
+	public static partial class Vector4Extensions
 	{
-		public static Vector4 InvertUVRectX(this Vector4 vector)
-		{
-			vector.z = Float.One - vector.z - vector.x;
-			return vector;
-		}
-
-		public static Vector4 InvertUVRectY(this Vector4 vector)
-		{
-			vector.w = Float.One - vector.w - vector.y;
-			return vector;
-		}
-
 		public static Rect ToRect(this Vector4 vector, Vector2 scale, Vector2 offset = default(Vector2))
 		{
 			return new Rect(
