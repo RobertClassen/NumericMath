4ebff386cf8a29e2ce5cace60b1b45167287cfe5
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-10-02 23:04:52 +0200

    added Angle method overload

--- Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector3/Vector3Extensions.Angle.cs
+++ Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector3/Vector3Extensions.Angle.cs
@@ -3,6 +3,7 @@ namespace WellDefinedValues
 	using System;
 	using System.Collections;
 	using System.Collections.Generic;
+	using Core;
 	using UnityEngine;
 
 	public static partial class Vector3Extensions
@@ -20,9 +21,29 @@ namespace WellDefinedValues
 		/// If set to <c>true</c> the returned angle is never greater than 180° or smaller than -180°.<br/>
 		/// If set to <c>false</c> the returned angle is never greater than 180°.<br/>
 		/// </param>
-		public static float Angle(this Vector3 from, Vector3 to, Vector3 axis, bool isSigned = Core.Numeric.IsAngleSignedDefault)
+		public static float Angle(this Vector3 from, Vector3 to, Vector3 axis, bool isSigned = Numeric.IsAngleSignedDefault)
 		{
 			return isSigned ? Vector3.SignedAngle(from, to, axis) : Vector3.Angle(from, to);
+		}
+
+		/// <summary>
+		/// Returns the signed or unsigned acute angle between the two vectors, in degrees.
+		/// </summary>
+		/// <remarks>
+		/// This means the smaller of the two possible angles between the two vectors is used.
+		/// 
+		/// See https://docs.unity3d.com/ScriptReference/Vector3.SignedAngle.html  
+		/// See https://docs.unity3d.com/ScriptReference/Vector3.Angle.html
+		/// </remarks>
+		/// <param name="isSigned">
+		/// If set to <c>true</c> the returned angle is never greater than 180 degrees or smaller than -180 degrees.<br/>
+		/// If set to <c>false</c> the returned angle is never greater than 180 degrees.<br/>
+		/// </param>
+		public static float Angle(this Vector3 from, Vector3 to, bool isSigned = Numeric.IsAngleSignedDefault)
+		{
+			from.Normalize();
+			to.Normalize();
+			return from.Dot(to) > -Float.One + Numeric.MagnitudeDelta ? from.Angle(to, from.Cross(to), isSigned) : 180f;
 		}
 	}
 }
