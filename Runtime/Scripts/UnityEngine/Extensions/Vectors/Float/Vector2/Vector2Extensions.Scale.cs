040cc2af8bbf5488e09ed1d15d8b3c4a8dcdfd43
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-10-03 14:42:59 +0200

    added Scale method overload

--- Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector2/Vector2Extensions.Scale.cs
+++ Runtime/Scripts/UnityEngine/Extensions/Vectors/Float/Vector2/Vector2Extensions.Scale.cs
@@ -12,6 +12,11 @@ namespace WellDefinedValues
 			return new Vector2(vector.x * x, vector.y);
 		}
 
+		public static Vector2 ScaleXY(this Vector2 vector, float x, float y)
+		{
+			return new Vector2(vector.x * x, vector.y * y);
+		}
+
 		/// <summary>
 		/// Scales both components of the vector.
 		/// </summary>
@@ -23,7 +28,7 @@ namespace WellDefinedValues
 		/// </remarks>
 		public static Vector2 ScaleXY(this Vector2 vector, Vector2 factor)
 		{
-			return new Vector2(vector.x * factor.x, vector.y * factor.y);
+			return vector.ScaleXY(factor.x, factor.y);
 		}
 
 		public static Vector2 ScaleY(this Vector2 vector, float y)
