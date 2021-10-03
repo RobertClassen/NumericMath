26e81c48c2b932b888d00c1dc5f4e680fa353fc4
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-10-03 17:50:00 +0200

    added Vector2 conversion methods

--- Runtime/Scripts/UnityEngine/Extensions/Rects/Float/RectExtensions.ToVector.cs
+++ Runtime/Scripts/UnityEngine/Extensions/Rects/Float/RectExtensions.ToVector.cs
@@ -5,7 +5,7 @@ namespace WellDefinedValues
 	using System.Collections.Generic;
 	using UnityEngine;
 
-	public static partial class Vector2Extensions
+	public static partial class RectExtensions
 	{
 		/// <summary>
 		/// Converts a relative vector to an absolute vector based on the <c>rect</c>, component-wise.
@@ -13,11 +13,10 @@ namespace WellDefinedValues
 		/// <remarks>
 		/// See https://docs.unity3d.com/ScriptReference/Rect.NormalizedToPoint.html
 		/// </remarks>
-		public static Vector2 ToAbsoluteXY(this Vector2 normalizedVector, Rect rect)
+		[Obsolete("Use 'normalizedVector.ToAbsoluteXY(rect)' instead.")]
+		public static Vector2 NormalizedToPoint(this Rect rect, Vector2 normalizedVector)
 		{
-			return new Vector2(
-				normalizedVector.x.Lerp(rect.x, rect.xMax), 
-				normalizedVector.y.Lerp(rect.y, rect.yMax));
+			return Rect.NormalizedToPoint(rect, normalizedVector);
 		}
 
 		/// <summary>
@@ -26,16 +25,10 @@ namespace WellDefinedValues
 		/// <remarks>
 		/// See https://docs.unity3d.com/ScriptReference/Rect.PointToNormalized.html
 		/// </remarks>
-		public static Vector2 ToRelativeXY(this Vector2 absoluteVector, Rect rect)
+		[Obsolete("Use 'absoluteVector.ToRelativeXY(rect)' instead.")]
+		public static Vector2 PointToNormalized(this Rect rect, Vector2 absoluteVector)
 		{
-			return new Vector2(
-				absoluteVector.x.InverseLerp(rect.x, rect.xMax), 
-				absoluteVector.y.InverseLerp(rect.y, rect.yMax));
-		}
-
-		public static Vector2Int ToVector2Int(this Vector2 vector)
-		{
-			return new Vector2Int((int)vector.x, (int)vector.y);
+			return Rect.PointToNormalized(rect, absoluteVector);
 		}
 	}
 }
