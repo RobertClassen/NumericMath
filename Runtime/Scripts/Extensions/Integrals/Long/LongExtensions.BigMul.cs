1faacf008bfccc1115d720f9316e848521400ed4
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-10-22 11:56:11 +0200

    added BigMul for long

---
+++ Runtime/Scripts/Extensions/Integrals/Long/LongExtensions.BigMul.cs
@@ -0,0 +1,15 @@
+namespace WellDefinedNumerics
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Numerics;
+
+	public static partial class LongExtensions
+	{
+		public static BigInteger BigMul(this long value, long factor)
+		{
+			return new BigInteger(value) * new BigInteger(factor);
+		}
+	}
+}
