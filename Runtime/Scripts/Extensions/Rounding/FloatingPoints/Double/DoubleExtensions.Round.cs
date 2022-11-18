131d36da6c14e947d482d4a2a4d9343f47c664b4
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2022-11-19 00:21:02 +0100

    added optional parameters to Round

--- Runtime/Scripts/Extensions/Rounding/FloatingPoints/Double/DoubleExtensions.Round.cs
+++ Runtime/Scripts/Extensions/Rounding/FloatingPoints/Double/DoubleExtensions.Round.cs
@@ -6,9 +6,9 @@ namespace NumericMath
 
 	public static partial class DoubleExtensions
 	{
-		public static double Round(this double value)
+		public static double Round(this double value, int digits = Int.Zero, MidpointRounding mode = MidpointRounding.ToEven)
 		{
-			return Math.Round(value);
+			return Math.Round(value, digits, mode);
 		}
 
 		public static long RoundToLong(this double value)
--- Runtime/Scripts/Extensions/Rounding/FloatingPoints/Float/FloatExtensions.Round.cs
+++ Runtime/Scripts/Extensions/Rounding/FloatingPoints/Float/FloatExtensions.Round.cs
@@ -6,9 +6,9 @@ namespace NumericMath
 
 	public static partial class FloatExtensions
 	{
-		public static float Round(this float value)
+		public static float Round(this float value, int digits = Int.Zero, MidpointRounding mode = MidpointRounding.ToEven)
 		{
-			return (float)Math.Round(value);
+			return (float)Math.Round(value, digits, mode);
 		}
 
 		public static int RoundToInt(this float value)
