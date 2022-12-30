d3eb0126b03a5caf2264e9c7715e48c77d5d4aef
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2022-12-30 21:04:37 +0100

    moved test methods

--- Tests/Runtime/Scripts/Int/IntTest.Modulo_Negative_Offset_Positive.cs
+++ Tests/Runtime/Scripts/Int/IntTest.Modulo_Negative_Offset_Positive.cs
@@ -9,6 +9,24 @@ namespace NumericMath
 	public partial class IntTest
 	{
 		[Test]
+		public void Modulo_Negative_Offset_Positive0()
+		{
+			int range = 5;
+			int[] input = (-range).Range(range * 2 + 1).ToArray();
+			Debug.Log(input);
+
+			int modulo = -3;
+			int offset = 0;
+			int[] expected = { 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2 };
+			Debug.Log(expected);
+
+			int[] actual = input.Select(value => value.Modulo(modulo, offset)).ToArray();
+			Debug.Log(actual);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
 		public void Modulo_Negative_Offset_Positive1()
 		{
 			int range = 5;
