b48f3d660b124df93b1c6b2b390c5319281df1be
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2023-01-01 19:52:39 +0100

    removed obsolete AreEqual method

--- Tests/Runtime/Scripts/Float/FloatTest.cs
+++ Tests/Runtime/Scripts/Float/FloatTest.cs
@@ -8,14 +8,5 @@ namespace NumericMath
 	public partial class FloatTest : NumericTest
 	{
 		private const float delta = 0.0001f;
-
-		private void AreEqual(IList<float> expected, IList<float> actual)
-		{
-			Assert.AreEqual(expected.Count, actual.Count);
-			for(int i = 0; i < expected.Count; i++)
-			{
-				Assert.AreEqual(expected[i], actual[i], delta, "Values differ at index [" + i + "]");
-			}
-		}
 	}
 }
