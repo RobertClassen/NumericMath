0ce4dd2b4fa94d7dc2a007fcca27cdd95fcb6e0f
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2023-01-01 20:09:37 +0100

    changed IntTest to derive from NumericTest

---
+++ Tests/Runtime/Scripts/Int/IntTest.cs
@@ -0,0 +1,11 @@
+namespace NumericMath
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using NUnit.Framework;
+
+	public partial class IntTest : NumericTest
+	{
+	}
+}
