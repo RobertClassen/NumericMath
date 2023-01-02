148f3946006921ad4c911fb3b6a4f4d533f0492b
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2023-01-02 01:09:48 +0100

    added StepSizes

--- Tests/Runtime/Scripts/Float/FloatTest.cs
+++ Tests/Runtime/Scripts/Float/FloatTest.cs
@@ -3,10 +3,12 @@ namespace NumericMath
 	using System;
 	using System.Collections;
 	using System.Collections.Generic;
-	using NUnit.Framework;
+	using System.Linq;
 
 	public partial class FloatTest : NumericTest
 	{
 		private const float delta = 0.0001f;
+
+		private static readonly float[] StepSizes = 10f.Divide(new []{ 2f, 3f, 4f, 5f, 10f }).Reverse().ToArray();
 	}
 }
