f8ff10f25fc960ce0e62e59e75a7ec6ca69bd1a2
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2023-01-02 15:31:22 +0100

    fixed tests

--- Tests/Runtime/Scripts/Float/FloatTest.RoundToNearest_Negative.cs
+++ Tests/Runtime/Scripts/Float/FloatTest.RoundToNearest_Negative.cs
@@ -25,94 +25,99 @@ namespace NumericMath
 		[Test]
 		public void RoundToNearest_NegativeValue_PositiveStepSize_Magnitude0_001()
 		{
-			const float value = -1234.5678f;
+			const float value = -1.2345678f;
+			Debug.Log(value, "Value");
 			const float scaleFactor = 0.001f;
-			Debug.Log(StepSizes.Multiply(scaleFactor));
+			Debug.Log(StepSizes.Multiply(scaleFactor), "Nearest");
 
-			throw new NotImplementedException();
-			float[] expected = { };
+			float[] expected = { -1.235f, -1.234f, -1.235f, -1.2333333f, -1.235f };
 			float[] actual = StepSizes.Multiply(scaleFactor).Select(stepSize => value.RoundToNearest(stepSize)).ToArray();
 
-			AreEqual(expected, actual, Delta);
+			AreEqual(expected, actual, scaleFactor * Delta);
 		}
 
 		[Test]
 		public void RoundToNearest_NegativeValue_PositiveStepSize_Magnitude0_01()
 		{
-			const float value = -1234.5678f;
+			const float value = -12.345678f;
+			Debug.Log(value, "Value");
 			const float scaleFactor = 0.01f;
-			Debug.Log(StepSizes.Multiply(scaleFactor));
+			Debug.Log(StepSizes.Multiply(scaleFactor), "Nearest");
 
-			throw new NotImplementedException();
-			float[] expected = { };
+			float[] expected = { -12.35f, -12.34f, -12.35f, -12.333333f, -12.35f };
 			float[] actual = StepSizes.Multiply(scaleFactor).Select(stepSize => value.RoundToNearest(stepSize)).ToArray();
 
-			AreEqual(expected, actual, Delta);
+			AreEqual(expected, actual, scaleFactor * Delta);
 		}
 
 		[Test]
 		public void RoundToNearest_NegativeValue_PositiveStepSize_Magnitude0_1()
 		{
-			const float value = -1234.5678f;
+			const float value = -123.45678f;
+			Debug.Log(value, "Value");
 			const float scaleFactor = 0.1f;
-			Debug.Log(StepSizes.Multiply(scaleFactor));
+			Debug.Log(StepSizes.Multiply(scaleFactor), "Nearest");
 
-			float[] expected = { -1234.6f, -1234.6f, -1234.666f, -1234.5f, -1234.5f };
+			float[] expected = { -123.5f, -123.4f, -123.5f, -123.33333f, -123.5f };
 			float[] actual = StepSizes.Multiply(scaleFactor).Select(stepSize => value.RoundToNearest(stepSize)).ToArray();
 
-			AreEqual(expected, actual, Delta);
+			AreEqual(expected, actual, scaleFactor * Delta);
 		}
 
 		[Test]
 		public void RoundToNearest_NegativeValue_PositiveStepSize_Magnitude1()
 		{
 			const float value = -1234.5678f;
+			Debug.Log(value, "Value");
 			const float scaleFactor = 1f;
-			Debug.Log(StepSizes.Multiply(scaleFactor));
+			Debug.Log(StepSizes.Multiply(scaleFactor), "Nearest");
 
-			float[] expected = { -1235, -1234, -1235, -1233, -1235 };
+			float[] expected = { -1235f, -1234f, -1235f, -1233.3333f, -1235f };
 			float[] actual = StepSizes.Multiply(scaleFactor).Select(stepSize => value.RoundToNearest(stepSize)).ToArray();
 
-			AreEqual(expected, actual, Delta);
+			AreEqual(expected, actual, scaleFactor * Delta);
 		}
 
 		[Test]
 		public void RoundToNearest_NegativeValue_PositiveStepSize_Magnitude10()
 		{
-			const float value = -1234.5678f;
+			const float value = -12345.678f;
+			Debug.Log(value, "Value");
 			const float scaleFactor = 10f;
-			Debug.Log(StepSizes.Multiply(scaleFactor));
+			Debug.Log(StepSizes.Multiply(scaleFactor), "Nearest");
 
-			float[] expected = { -1230, -1240, -1225, -1233, -1250 };
+			float[] expected = { -12350f, -12340f, -12350f, -12333.333f, -12350f };
 			float[] actual = StepSizes.Multiply(scaleFactor).Select(stepSize => value.RoundToNearest(stepSize)).ToArray();
 
-			AreEqual(expected, actual, Delta);
+			AreEqual(expected, actual, scaleFactor * Delta);
 		}
 
 		[Test]
 		public void RoundToNearest_NegativeValue_PositiveStepSize_Magnitude100()
 		{
-			const float value = -1234.5678f;
+			const float value = -123456.78f;
+			Debug.Log(value, "Value");
 			const float scaleFactor = 100f;
-			Debug.Log(StepSizes.Multiply(scaleFactor));
+			Debug.Log(StepSizes.Multiply(scaleFactor), "Nearest");
 
-			float[] expected = { -1200, -1200, -1250, -1333, -1000 };
+			float[] expected = { -123500f, -123400f, -123500f, -123333.33f, -123500f };
 			float[] actual = StepSizes.Multiply(scaleFactor).Select(stepSize => value.RoundToNearest(stepSize)).ToArray();
 
-			AreEqual(expected, actual, Delta);
+			AreEqual(expected, actual, scaleFactor * Delta);
 		}
 
 		[Test]
 		public void RoundToNearest_NegativeValue_PositiveStepSize_Magnitude1000()
 		{
-			const float value = -1234.5678f;
+			const float value = -1234567.8f;
+			Debug.Log(value, "Value");
 			const float scaleFactor = 1000f;
-			Debug.Log(StepSizes.Multiply(scaleFactor));
+			Debug.Log(StepSizes.Multiply(scaleFactor), "Nearest");
 
-			float[] expected = { -1000, -2000, 0, 0, 0 };
+			float[] expected = { -1235000f, -1234000f, -1235000f, -1233333.3f, -1235000f };
 			float[] actual = StepSizes.Multiply(scaleFactor).Select(stepSize => value.RoundToNearest(stepSize)).ToArray();
 
-			AreEqual(expected, actual, Delta);
+			AreEqual(expected, actual, scaleFactor * Delta);
 		}
 	}
 }
