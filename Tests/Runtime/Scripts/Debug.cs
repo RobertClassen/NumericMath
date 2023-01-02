86e549c72d3265551da4c71b6f743066a4341a56
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2023-01-02 12:43:01 +0100

    changed Log methods to accept name parameter instead of separator; simplified code

--- Tests/Runtime/Scripts/Debug.cs
+++ Tests/Runtime/Scripts/Debug.cs
@@ -7,25 +7,29 @@ namespace NumericMath
 	public static class Debug
 	{
 		private const string SeparatorDefault = ", ";
+		private static readonly string[] Tabs = { "\t\t", "\t" };
+		private const int LengthPerTab = 8;
 
-		public static void Log(string value)
+		public static void Log(string value, string name = null)
 		{
-			UnityEngine.Debug.Log(value);
+			UnityEngine.Debug.Log(string.IsNullOrEmpty(name)
+				? value
+				: string.Format("{0}:{1}{2}", name, Tabs[(name.Length / LengthPerTab).Clamp(Tabs)], value));
 		}
 
-		public static void Log(object value)
+		public static void Log(object value, string name = null)
 		{
-			UnityEngine.Debug.Log(value);
+			Log(value.ToString(), name);
 		}
 
-		public static void Log<T>(IList<T> iList, string separator = SeparatorDefault)
+		public static void Log<T>(IList<T> iList, string name = null)
 		{
-			UnityEngine.Debug.Log(string.Join(separator, iList));
+			Log(string.Join(SeparatorDefault, iList), name);
 		}
 
-		public static void Log<T>(IEnumerable<T> iEnumerable, string separator = SeparatorDefault)
+		public static void Log<T>(IEnumerable<T> iEnumerable, string name = null)
 		{
-			UnityEngine.Debug.Log(string.Join(separator, iEnumerable));
+			Log(string.Join(SeparatorDefault, iEnumerable), name);
 		}
 	}
 }
