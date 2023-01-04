7744062b9f995261df480930a539dffc6be2acab
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2023-01-04 15:19:49 +0100

    added check for zero in RoundBetween

--- Runtime/Scripts/Extensions/Rounding/_Double/DoubleExtensions.RoundBetweenMinToMax.cs
+++ Runtime/Scripts/Extensions/Rounding/_Double/DoubleExtensions.RoundBetweenMinToMax.cs
@@ -15,6 +15,10 @@ namespace NumericMath
 		public static double RoundBetweenMinToMax(this double value, double stepSize, double percent, 
 			bool clamp = Numeric.IsLerpClampedDefault)
 		{
+			if(stepSize.IsZero())
+			{
+				throw new DivideByZeroException(nameof(stepSize));
+			}
 			double fraction = value / stepSize;
 			return percent.Lerp(Math.Floor(fraction), Math.Ceiling(fraction), clamp) * stepSize;
 		}
--- Runtime/Scripts/Extensions/Rounding/_Double/DoubleExtensions.RoundBetweenNearestToFurthest.cs
+++ Runtime/Scripts/Extensions/Rounding/_Double/DoubleExtensions.RoundBetweenNearestToFurthest.cs
@@ -16,6 +16,10 @@ namespace NumericMath
 		public static double RoundBetweenNearestToFurthest(this double value, double stepSize, double percent, 
 			bool clamp = Numeric.IsLerpClampedDefault)
 		{
+			if(stepSize.IsZero())
+			{
+				throw new DivideByZeroException(nameof(stepSize));
+			}
 			double fraction = value / stepSize;
 			double floor = Math.Floor(fraction);
 			double ceiling = Math.Ceiling(fraction);
